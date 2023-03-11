466f40d3e0d923ba4a72e2ff64e2675514e6cc2d
michaelrilan <micorilan1999@gmail.com>
2023-03-11 11:36:42 +0800

    successfully refreshed my mind HEHEHEHE

--- custom_print_mainapp/views.py
+++ custom_print_mainapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('fddfdsfsdfdsf')
+      return render(request, 'index.html')
